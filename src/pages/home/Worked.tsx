9f220d76c3acc3a96ee3171f6653e435e65ae053
draw-n <helen.wu1219@gmail.com>
2025-06-20 11:01:16 +0800

    added companies

--- src/pages/home/Worked.tsx
+++ src/pages/home/Worked.tsx
@@ -1,17 +1,36 @@
-import { Flex, Title, Card, ThemeIcon, CheckIcon, Text } from "@mantine/core";
+import { Flex, Title, Image, Grid, Container } from "@mantine/core";
 
 const Worked: React.FC = () => {
+    const companies = [
+        "/companies/Amazon-Logo-2000.png",
+        "/companies/Apple-Logosu.png",
+        "/companies/Capital_One_logo.svg.png",
+        "/companies/DoorDash-logo.png",
+        "/companies/Fidelity-Logo.png",
+        "/companies/Google_2015_logo.svg.png",
+        "/companies/Jane_Street.png",
+        "/companies/Meta-Logo.png",
+        "/companies/MongoDB_Logo.svg.png",
+        "/companies/Pinterest_Logo.svg.png",
+        "/companies/purepng.com-microsoft-logologobrand-logoiconslogos-251519939132du80p.png",
+        "/companies/Salesforce.com_logo.svg.png",
+    ];
     return (
-        <Flex
-            c="black"
-            bg="sky"
-            gap="xl"
-            p="xl"
-            direction={{ base: "column", sm: "row" }}
-        >
+        <Flex c="black" bg="sky" gap="xl" p="xl" direction="column">
             <Title order={2} ta="center">
                 Where our members have worked
             </Title>
+            <Container>
+                <Grid gutter={20}>
+                    {companies.map((company) => (
+                        <Grid.Col span={{ base: 6, sm: 3 }}>
+                            <Flex align="center" h="100%">
+                                <Image src={company} />
+                            </Flex>
+                        </Grid.Col>
+                    ))}
+                </Grid>
+            </Container>
         </Flex>
     );
 };
