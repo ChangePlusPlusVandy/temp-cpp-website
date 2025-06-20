e391426b506c46f84d6e9643a45780f09bd1e65c
draw-n <helen.wu1219@gmail.com>
2025-06-20 11:07:25 +0800

    pushing

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/temp-cpp-website",
   resolve: {
+    
     alias: {
       // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
       '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
